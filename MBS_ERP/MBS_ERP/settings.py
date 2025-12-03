c7bf20f3e284a1f938adb343a1ea77da3fc24ec9
hfizhirfan <hafizh.irfansyah41@gmail.com>
2025-12-03 16:07:11 +0700

    coba custom

--- MBS_ERP/MBS_ERP/settings.py
+++ MBS_ERP/MBS_ERP/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'MBS_ERP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
