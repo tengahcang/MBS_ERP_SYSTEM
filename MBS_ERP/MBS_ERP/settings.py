c64d972c532d7d607035348c846927190f3c2780
tengah cang <cangg522@gmail.com>
2025-12-04 09:24:10 +0700

    update auto terisi log dari perubahan harga jual product atau harga jual vendors

--- MBS_ERP/MBS_ERP/settings.py
+++ MBS_ERP/MBS_ERP/settings.py
@@ -11,9 +11,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^bn1(d6)ed@is!-21wumj18$e@i(v#vbo#bjty1uf*rcrq6fse'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -57,6 +67,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / "templates"],
+        # 'DIRS' : [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,14 +92,25 @@ WSGI_APPLICATION = 'MBS_ERP.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mbs_erp',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306'
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mbs_erp',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
@@ -127,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- MBS_ERP/core/admin.py
+++ MBS_ERP/core/admin.py
@@ -34,14 +34,71 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ("product_code", "name", "brand", "category", "description", "selling_price", "is_active")
     list_filter = ("brand", "category", "is_active")
     search_fields = ("product_code", "name")
-
+    
     inlines = [ProductVendorInline]
+    
+    def save_model(self, request, obj, form, change):
+        """
+        Override penyimpanan product untuk mendeteksi perubahan selling_price
+        dan menambah ke ProductChangeLog otomatis.
+        """
+        if change:  # hanya berjalan saat update, bukan create
+            old_product = Product.objects.get(pk=obj.pk)
+            old_price = old_product.selling_price
+            new_price = form.cleaned_data.get("selling_price")
+            if old_price != new_price:
+                # Buat log otomatis
+                ProductChangeLog.objects.create(
+                    product=obj,
+                    changed_by=request.user,
+                    old_value=str(old_price),
+                    new_value=str(new_price)
+                )
+        super().save_model(request, obj, form, change)
 
+    def save_related(self, request, form, formsets, change):
+        """
+        Detect perubahan capital_price pada inline VendorProduct
+        dan otomatis membuat log VendorProductPricingLog.
+        """
+        for formset in formsets:
+            if formset.model == VendorProduct:
+                # loop semua inline VendorProduct
+                for inline_form in formset.forms:
+                    if not inline_form.cleaned_data:
+                        continue
+                    obj = inline_form.instance
+                    if obj.pk:  # hanya update
+                        old = VendorProduct.objects.get(pk=obj.pk)
+                        old_price = old.capital_price
+                        new_price = inline_form.cleaned_data.get("capital_price")
+                        if old_price != new_price:
+                            VendorProductPricingLog.objects.create(
+                                vendor_product=obj,
+                                changed_by=request.user,
+                                old_value=old_price,
+                                new_value=new_price
+                            )
+
+        super().save_related(request, form, formsets, change)
 
 @admin.register(VendorProduct)
 class VendorProductAdmin(admin.ModelAdmin):
     list_display = ("product", "vendor", "vendor_sku", "capital_price")
-    search_fields = ("product__name", "vendor__vendor_name", "vendor_sku")
+    search_fields = ("product__name", "vendor__name", "vendor_sku")
+    def save_model(self, request, obj, form, change):
+        if change:  # hanya update
+            old_obj = VendorProduct.objects.get(pk=obj.pk)
+            old_price = old_obj.capital_price
+            new_price = form.cleaned_data.get("capital_price")
+            if old_price != new_price:
+                VendorProductPricingLog.objects.create(
+                    vendor_product=obj,
+                    changed_by=request.user,
+                    old_value=old_price,
+                    new_value=new_price
+                )
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(ProductChangeLog)
