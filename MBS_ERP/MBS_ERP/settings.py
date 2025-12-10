472d0e1d573c336c57d35db405b7a3ddd1aa777b
tengah cang <cangg522@gmail.com>
2025-12-10 08:27:27 +0700

    update untuk vercel

--- MBS_ERP/MBS_ERP/settings.py
+++ MBS_ERP/MBS_ERP/settings.py
@@ -165,9 +165,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "MBS_ERP/static"),
+# ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storege.CompressedManifestStaticFileStorage"
+
+
 # STATIC_URL = 'static/'
 
 # Default primary key field type
--- MBS_ERP/vercel_handler.py
+++ MBS_ERP/vercel_handler.py
@@ -1,6 +1,6 @@
 import os
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MBS_ERP.settings")
 
-app = get_asgi_application()
+app = get_wsgi_application()
